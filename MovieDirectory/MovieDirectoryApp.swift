e5298a2aa2e2341e2c6a459c3e5ea23cc3087ecf
Michael Tjokrowardojo <mtjokrow@sfu.ca>
2025-01-30 13:47:51 -0800

    Initial Commit

---
+++ MovieDirectory/MovieDirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDirectoryApp.swift
+//  MovieDirectory
+//
+//  Created by Michael Tjokrowardojo on 2025-01-30.
+//
+
+import SwiftUI
+
+@main
+struct MovieDirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
