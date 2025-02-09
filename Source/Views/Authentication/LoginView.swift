9797c4ff02df1db9b8e0f8f8f4328df0b1bbf7f7
MichaelTj02 <mtjokrowardojo@gmail.com>
2025-02-09 14:17:14 -0800

    fix file structure

---
+++ Source/Views/Authentication/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  MovieDirectory
+//
+//  Created by Michael Tjokrowardojo on 2025-01-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
