1fbb094d9e03639114de3e0cf103f976b8d33840
MichaelTj02 <mtjokrowardojo@gmail.com>
2025-02-09 21:30:10 -0800

    setup firebase

---
+++ Source/Views/Authentication/AuthViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  AuthViewModel.swift
+//  MovieDirectory
+//
+//  Created by Michael Tjokrowardojo on 2025-02-09.
+//
+
--- Source/Views/Authentication/LoginView.swift
+++ Source/Views/Authentication/LoginView.swift
@@ -11,6 +11,8 @@ struct LoginView: View {
     var body: some View {
         VStack {
             Text("Login Page")
+            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: username)
+            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
         }
     }
 }
