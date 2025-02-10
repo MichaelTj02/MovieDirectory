6c4568bf9794ea0c58281186c2b88e8f2b2cec71
MichaelTj02 <mtjokrowardojo@gmail.com>
2025-02-09 23:16:18 -0800

    fix firebase issues

--- Source/ViewModels/AuthViewModel.swift
+++ Source/ViewModels/AuthViewModel.swift
@@ -5,3 +5,15 @@
 //  Created by Michael Tjokrowardojo on 2025-02-09.
 //
 
+import SwiftUI
+import FirebaseAuth
+
+class AuthViewModel: ObservableObject {
+    @Published var user: User? = nil
+    @Published var errorMessage: String = ""
+    
+    init() {
+        self.user = Auth.auth().currentUser
+    }
+}
+
